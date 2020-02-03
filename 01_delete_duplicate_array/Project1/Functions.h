0bc9cb7817590255adab45b9eda0461789c87f99
williamblood <w.blood@icloud.com>
2020-02-02 20:09:21 -0800

    bugfix: algorithm now correctly moves to the next item if a duplicate is not found

--- 01_delete_duplicate_array/Project1/Functions.h
+++ 01_delete_duplicate_array/Project1/Functions.h
@@ -26,16 +26,18 @@ void removeNumber(int a[], int& numOfElem, int idx) {
 }
 
 void deleteDuplicates(int a[], int& numOfElem) {
-
-	for (int i = 0; i < numOfElem; ++i) {
-		for (int j = 0; j < i; ++j) {
+	for (int i = 0; i < numOfElem; i++) {
+		cout << "checking for duplicates of " << a[i] << endl;
+		for (int j = i + 1; j < numOfElem;) {
+			cout << "looking at " << a[j] << endl;
 			if (a[i] == a[j]) {
-				cout << "\nDuplicate found. Deleting [" << a[i] << "]\n";
-				removeNumber(a, numOfElem, j);
+				cout << "duplicate found. deleting [" << a[j] << "]\n";
+				numOfElem--;
+				for (int r = j; r < numOfElem; r++) {
+					a[j] = a[j + 1];
+				}
 			}
+			else ++j;
 		}
 	}
-	cout << "Finished\nTotal remaining elements: " << numOfElem << endl;
-	cout << endl;
 }
-

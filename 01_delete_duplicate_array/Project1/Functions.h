40b1c724766339069a5d3a8956caf19cfe2af2ca
William Blood <william.blood@icloud.com>
2020-02-06 12:52:08 -0800

    Optimized for readability

--- 01_delete_duplicate_array/Project1/Functions.h
+++ 01_delete_duplicate_array/Project1/Functions.h
@@ -14,30 +14,24 @@ void removeNumber(int a[], int& numOfElem, int idx);
 void deleteDuplicates(int a[], int& numOfElem);
 
 void printArray(int a[], int numOfElem) {
-	for (int i = 0; i < numOfElem; i++)
+	for (int i = 0; i < numOfElem; ++i)
 		cout << a[i] << " ";
 }
 
-void removeNumber(int a[], int& numOfElem, int idx) {
-	numOfElem--;
-	for (int k = idx; k < numOfElem; k++) {
-		a[k] = a[k + 1];
+void deleteDuplicates(int a[], int& numOfElem) {
+	for (int i = 0; i < numOfElem; ++i) {	// Standard iterator
+		int nextIndex = i + 1;			    // Location of possible duplicate
+		while (nextIndex < numOfElem) {		// Linear search and destroy algorithm
+			if (a[i] == a[nextIndex])
+				removeNumber(a, numOfElem, nextIndex);
+			else 
+				++nextIndex;				// Advances to the next suspect
+		}
 	}
 }
 
-void deleteDuplicates(int a[], int& numOfElem) {
-	for (int i = 0; i < numOfElem; i++) {
-		cout << "checking for duplicates of " << a[i] << endl;
-		for (int j = i + 1; j < numOfElem;) {
-			cout << "looking at " << a[j] << endl;
-			if (a[i] == a[j]) {
-				cout << "duplicate found. deleting [" << a[j] << "]\n";
-				for (int r = j; r < numOfElem; r++) {
-					a[r] = a[r + 1];
-				}
-				numOfElem--;
-			}
-			else ++j;
-		}
-	}
+void removeNumber(int a[], int& numOfElem, int idx) {
+	numOfElem--;							// Updates size
+	for (int k = idx; k < numOfElem; k++)	// Replaces current element with next
+		a[k] = a[k + 1];
 }

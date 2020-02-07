7d14e411660ff604dfc1b7c253af51047ebe50f7
William Blood <william.blood@icloud.com>
2020-02-07 14:08:40 -0800

    cleaner comments

--- 01_delete_duplicate_array/Project1/Functions.h
+++ 01_delete_duplicate_array/Project1/Functions.h
@@ -9,29 +9,25 @@
 
 #include <iostream>
 
-void printArray(int a[], int numOfElem);
-void removeNumber(int a[], int& numOfElem, int idx);
-void deleteDuplicates(int a[], int& numOfElem);
+void unshift(int a[], int& numOfElem, int idx) {
+	numOfElem--;							// Reduces array size by 1
+	for (int k = idx; k < numOfElem; ++k)	// Shifts all elems from idx pos.
+		a[k] = a[k + 1];
+}
 
 void printArray(int a[], int numOfElem) {
-	for (int i = 0; i < numOfElem; ++i)
+	for (int i = 0; i < numOfElem; ++i)		// Prints elems to cout stream
 		cout << a[i] << " ";
 }
 
 void deleteDuplicates(int a[], int& numOfElem) {
 	for (int i = 0; i < numOfElem; ++i) {	// Standard iterator
 		int nextIndex = i + 1;			    // Location of possible duplicate
-		while (nextIndex < numOfElem) {		// Linear search and destroy algorithm
-			if (a[i] == a[nextIndex])
-				removeNumber(a, numOfElem, nextIndex);
+		while (nextIndex < numOfElem) {		// Begins searching from dupe pos.
+			if (a[i] == a[nextIndex])		// Removes duplicate when encountered
+				unshift(a, numOfElem, nextIndex);
 			else 
-				++nextIndex;				// Advances to the next suspect
+				++nextIndex;				// Advances to the next element
 		}
 	}
-}
-
-void removeNumber(int a[], int& numOfElem, int idx) {
-	numOfElem--;							// Updates size
-	for (int k = idx; k < numOfElem; k++)	// Replaces current element with next
-		a[k] = a[k + 1];
 }

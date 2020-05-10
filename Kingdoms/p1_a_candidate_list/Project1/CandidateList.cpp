78777a8a24c6cf7a6aa8bfb94eb33eb67e916cce
williamblood <w.blood@icloud.com>
2020-05-10 02:05:37 -0700

    optimized

--- Kingdoms/p1_a_candidate_list/Project1/CandidateList.cpp
+++ Kingdoms/p1_a_candidate_list/Project1/CandidateList.cpp
@@ -1,15 +1,13 @@
 /*
-    KaibaCorp
-    Bearden, Reese (TTh 6:30)
-    Blood, William (TTh 6:30)
-    Diep, Vince (TTh 2:20)
-    Huynh, Andy (TTh 6:30)
-    Nguyen, Andrew (MW 11:10)
-
-    May 05, 2020
-
-    CS A250
-    Project 1 - Part B
+	KaibaCorp
+	Bearden, Reese (TTh 6:30)
+	Blood, William (TTh 6:30)
+	Diep, Vince (TTh 2:20)
+	Huynh, Andy (TTh 6:30)
+	Nguyen, Andrew (MW 11:10)
+	May 05, 2020
+	CS A250
+	Project 1 - Part B
 */
 
 #include "CandidateList.h"
@@ -28,7 +26,7 @@ CandidateList::CandidateList()
 }
 
 // addCandidate
-void CandidateList::addCandidate(const CandidateType& candidate) 
+void CandidateList::addCandidate(const CandidateType& candidate)
 {
 	Node* temp = new Node(candidate, nullptr);
 
@@ -48,11 +46,11 @@ void CandidateList::addCandidate(const CandidateType& candidate)
 }
 
 // getWinner
-int CandidateList::getWinner() const 
+int CandidateList::getWinner() const
 {
 	if (count == 0)
 	{
-		cerr << "=> List is empty.";
+		cerr << "    => List is empty.";
 		return 0;
 	}
 	else
@@ -60,59 +58,53 @@ int CandidateList::getWinner() const
 		Node* temp = first;
 		int top = 0;
 		int idStore = 0;
-
-		while (temp != last)
+		while (temp != nullptr)
 		{
-			if (temp->getCandidate().getTotalVotes() > top)
+			const int voteCount = temp->getCandidate().getTotalVotes();
+			if (voteCount > top)
 			{
-				top = temp->getCandidate().getTotalVotes();
+				top = voteCount;
 				idStore = temp->getCandidate().getID();
 			}
 			temp = temp->getLink();
 		}
 
-		temp = nullptr;
 		return idStore;
 	}
 }
 
 // isEmpty
-bool CandidateList::isEmpty() const 
+bool CandidateList::isEmpty() const
 {
 	return (count == 0);
 }
 
 // searchCandidate
-bool CandidateList::searchCandidate(int id) const 
+bool CandidateList::searchCandidate(int id) const
 {
 	Node* temp = nullptr;
 	return searchCandidate(id, temp);
 }
 
-bool CandidateList::searchCandidate(int id, Node*& ptr) const 
+bool CandidateList::searchCandidate(int id, Node*& ptr) const
 {
 	Node* temp = first;
-	bool found = false;
-	while (temp != nullptr && !found)
+	while (temp != nullptr)
 	{
 		if (temp->getCandidate().getID() == id)
 		{
 			ptr = temp;
-			found = true;
-			return found;
+			return true;
 		}
 		temp = temp->getLink();
 	}
+	cerr << "    => ID not in the list." << endl;
 
-	if (!found)
-	{
-		cerr << "    => ID not in the list." << endl;
-	}
-	return found;
+	return false;
 }
 
 // printCandidateName
-void CandidateList::printCandidateName(int id) const 
+void CandidateList::printCandidateName(int id) const
 {
 	Node* temp = nullptr;
 	if (searchCandidate(id, temp))
@@ -122,7 +114,7 @@ void CandidateList::printCandidateName(int id) const
 }
 
 // printAllCandidates
-void CandidateList::printAllCandidates() const 
+void CandidateList::printAllCandidates() const
 {
 	Node* temp = first;
 	while (temp != nullptr)
@@ -134,19 +126,19 @@ void CandidateList::printAllCandidates() const
 }
 
 // printKingdomVotes
-void CandidateList::printKingdomVotes(int id, int index) const 
+void CandidateList::printKingdomVotes(int id, int index) const
 {
 	Node* temp = nullptr;
 	if (searchCandidate(id, temp))
 	{
-		cout << "    *" << right << setw(3) 
+		cout << "    *" << right << setw(3)
 			<< temp->getCandidate().getVotesByKingdom(index)
 			<< "( => )" << KINGDOMS[index] << endl;
 	}
 }
 
 // printCandidateTotalVotes
-void CandidateList::printCandidateTotalVotes(int id) const 
+void CandidateList::printCandidateTotalVotes(int id) const
 {
 	Node* temp = nullptr;
 	if (searchCandidate(id, temp))
@@ -157,10 +149,8 @@ void CandidateList::printCandidateTotalVotes(int id) const
 }
 
 // printFinalResults
-void CandidateList::printFinalResults() const 
+void CandidateList::printFinalResults() const
 {
-	Node* temp = first;
-	
 	cout << "************ FINAL RESULTS ************" << endl << endl;
 
 	cout << left << setw(15) << "LAST" << left << setw(10) << "FIRST"
@@ -171,57 +161,60 @@ void CandidateList::printFinalResults() const
 
 	cout << "_______________________________________" << endl << endl;
 
-
-	//int max = temp->getCandidate().getTotalVotes();
-	//int maxId[31];	// All possible candidates SORTED
-	//
-	//for (int i = 0; temp->getLink() != nullptr;)
-	//{
-	//	if (max > temp->getLink()->getCandidate().getTotalVotes())
-	//	{
-	//		max = temp->getCandidate().getTotalVotes();
-	//		maxId[i] = temp->getCandidate().getID();	// stores max ID
-	//		++i;
-	//	}
-	//	else
-	//	{
-	//		temp = temp->getLink();
-	//	}
-	//
-	//}
-	
-	//temp = first; // RESET
-
-	int pos = 0;
-	int i = 0;
-	while (temp != nullptr)
+	Node* winner = first;
+	int prevHighestVoteCount(0);
+	while (winner != nullptr)
 	{
-		++pos;
-		++i;
-		cout << left << setw(15) << temp->getCandidate().getLastName()
-			<< left << setw(10) << temp->getCandidate().getFirstName()
-			<< right << setw(5) << temp->getCandidate().getTotalVotes()
+		const int prevVoteCount = winner->getCandidate().getTotalVotes();
+		if (prevVoteCount > prevHighestVoteCount)
+		{
+			prevHighestVoteCount = prevVoteCount;
+		}
+		winner = winner->getLink();
+	}
+
+	++prevHighestVoteCount;
+
+	for (int pos = 1; pos <= count; ++pos)
+	{
+		Node* temp = first;
+		int highestVoteCount = 0;
+
+		while (temp != nullptr)
+		{
+			const int tempVotes = temp->getCandidate().getTotalVotes();
+			if (tempVotes > highestVoteCount &&
+				tempVotes < prevHighestVoteCount)
+			{
+				highestVoteCount = tempVotes;
+				winner = temp;
+			}
+			temp = temp->getLink();
+		}
+		prevHighestVoteCount = highestVoteCount;
+
+		cout << left << setw(15) << winner->getCandidate().getLastName()
+			<< left << setw(10) << winner->getCandidate().getFirstName()
+			<< right << setw(5) << highestVoteCount
 			<< right << setw(7) << pos << endl;
-	
-		temp = temp->getLink();
-		
-		if (i % 5 == 0)
+
+		if (pos % 5 == 0)
 		{
 			cout << "---------------------------------------" << "\n";
-		}	
+		}
 	}
 
 	cout << "_______________________________________" << endl;
 }
 
 // clearList
-void CandidateList::clearList() 
+void CandidateList::clearList()
 {
 	Node* temp = first;
 	while (temp != last)
 	{
 		temp = temp->getLink();
-		delete first; 
+		delete first;
 		first = temp;
 	}
 
@@ -229,7 +222,7 @@ void CandidateList::clearList()
 }
 
 // Destructor
-CandidateList::~CandidateList() 
+CandidateList::~CandidateList()
 {
 	clearList();
 }

52a0b703b796faec16d4e67c4cb6a3328d265a33
William Blood <william.blood@icloud.com>
2020-05-15 18:45:30 -0700

    project 2 - initial commit

---
+++ Kingdoms/Mac/Project2/Project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by William Blood on 5/14/20.
+//  Copyright © 2020 William Blood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
